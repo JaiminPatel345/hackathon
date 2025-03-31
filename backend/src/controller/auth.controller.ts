f426a40bc2672f9049736fed8f98cfef51b21eb3
Jaimin Detroja <124363976+JaiminPatel345@users.noreply.github.com>
2025-03-31 14:24:39 +0530

    Signup with Password

--- backend/src/controller/auth.controller.ts
+++ backend/src/controller/auth.controller.ts
@@ -1,10 +1,18 @@
 import {Request, Response} from "express";
 import {AppError, formatResponse} from "../types/custom.types.js";
-import {ILoginRequest, IRegisterRequest} from "../types/request.types.js";
+import {
+  ILoginRequest,
+  IRegisterRequest,
+  IVerifyOtpRequest
+} from "../types/request.types.js";
 import User from "../model/user.model.js";
 import {generateHashPassword, verifyPassword} from "../utiles/hashPassword.js";
 import handleError from "../utiles/handleError.js";
 import handleOtp from "../utiles/handleOtp.js";
+import {getMobileAndOtp, incrementWrongAttempts} from "../redis/redisUtils.js";
+import {getTokens} from "../utiles/tokens.js";
+
+const MAX_WRONG_OTP_COUNT = 5;
 
 export const loginUser = async (req: Request, res: Response) => {
   try {
@@ -14,7 +22,9 @@ export const loginUser = async (req: Request, res: Response) => {
       hasMobile = true;
     }
 
-    const user = await User.findOne(hasMobile ? {mobile: identifier} : {username: identifier}).lean()
+    const user = await User.findOne(hasMobile ? {mobile: identifier} : {username: identifier})
+        .select('+hashPassword')
+        .lean()
     if (!user) {
       console.log("User not found");
       throw new AppError("Invalid credentials", 401)
@@ -26,6 +36,13 @@ export const loginUser = async (req: Request, res: Response) => {
       console.log("Wrong Password");
       throw new AppError("Invalid credentials", 401)
     }
+
+    const token = await getTokens(user.username)
+
+    const {hashPassword, ...userWithoutPassword} = user;
+
+    res.json(formatResponse(true, "Login Successful",
+        {user: userWithoutPassword, token}));
 
 
   } catch (error: any) {
@@ -42,7 +59,7 @@ export const registerUser = async (req: Request, res: Response) => {
     const hashPassword = await generateHashPassword(password);
 
     //generate and set opt
-    await handleOtp(mobile);
+    await handleOtp(mobile, username);
 
 
     const user = await User.create({
@@ -53,12 +70,44 @@ export const registerUser = async (req: Request, res: Response) => {
       hashPassword,
     })
 
-    res.status(201).json(formatResponse(true,'Created successfully' , {user}));
+    res.status(201).json(formatResponse(true, 'Created successfully', {user}));
 
 
   } catch (error) {
     console.log("Error at registerUser");
     handleError(error, res);
 
+  }
+}
+
+export const verifyOtp = async (req: Request, res: Response) => {
+  try {
+    const {username, givenOtp}: IVerifyOtpRequest = req.body;
+    const RedisData = await getMobileAndOtp(username);
+    if (!RedisData) {
+      console.log("RedisData not found");
+      throw new AppError("Try to resend OTP", 401)
+    }
+    if (RedisData.generatedOtp.toString() === givenOtp.toString()) {
+      const user = await User.findOneAndUpdate({
+        username,
+        isMobileVerified: true
+      })
+      const token = await getTokens(username)
+
+      res.status(200).json(formatResponse(true, "OTP verified", {user, token}));
+      return;
+    } else {
+      if (RedisData.wrongAttempts >= MAX_WRONG_OTP_COUNT) {
+        throw new AppError("Wrong attempts and Max attempts reached ", 401)
+
+      }
+      await incrementWrongAttempts(username);
+      throw new AppError("Wrong OTP , Try again", 401)
+    }
+
+  } catch (error) {
+    console.log("Error at verifyOtp");
+    handleError(error, res);
   }
 }
--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -2,11 +2,12 @@ import express from 'express'
 const router = express.Router()
 import * as authController from '../controller/auth.controller.js'
 import {
-  verifyLoginRequest,
+  verifyLoginRequest, verifyOtpRequest,
   verifySignupRequest
 } from "../utiles/verifyRequest/authRequest.js";
 
 router.post('/login',verifyLoginRequest, authController.loginUser)
 router.post('/register',verifySignupRequest, authController.registerUser)
+router.post('/verify-otp' , verifyOtpRequest, authController.verifyOtp)
 
 export default router
